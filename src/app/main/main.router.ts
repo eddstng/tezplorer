b31592114c9c6cf0638c6445389ae8ec77ff975c
eddstng <eddstng@gmail.com>
2022-02-20 20:29:29 -0800

    added getOperationsFromAddressDescAfter and get /operations/:address/:relationshipType/after/:cursor

--- src/app/main/main.router.ts
+++ src/app/main/main.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getOperationsFromAddressDesc } from './main';
+import { getOperationsFromAddressDesc, getOperationsFromAddressDescAfter } from './main';
 export const router: Router = Router();
 
 router.get('/operations/:address/:relationshipType', async (req, res) => {
@@ -9,6 +9,19 @@ router.get('/operations/:address/:relationshipType', async (req, res) => {
       res.status(400).send("Must declare either 'destination' or 'source' as relationshipType. \n `/operations/:address/:relationshipType`")
     } else {
       res.status(200).send(await getOperationsFromAddressDesc(req.params.address, relationshipType));
+    }
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+router.get('/operations/:address/:relationshipType/after/:cursor', async (req, res) => {
+  try {
+    const relationshipType = req.params.relationshipType
+    if (relationshipType !== 'destination' && relationshipType !== 'source') {
+      res.status(400).send("Must declare either 'destination' or 'source' as relationshipType. \n `/operations/:address/:relationshipType`")
+    } else {
+      res.status(200).send(await getOperationsFromAddressDescAfter(req.params.address, relationshipType, req.params.cursor));
     }
   } catch (error) {
     console.log(error)
